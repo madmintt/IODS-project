c38c548f285f4ccb4488294c4f7d0724464ecdc2
minttu lindholm <minttu.lindholm@helsinki.fi>
2019-11-08 09:33:24 +0200

    commit

---
+++ Data/create_learning2014.R
@@ -0,0 +1,34 @@
+#Minttu Lindholm 7.11.2019
+#Data wrangling
+
+install.packages("dplyr")
+library(dplyr)
+
+lrn14 <- read.table("http://www.helsinki.fi/~kvehkala/JYTmooc/JYTOPKYS3-data.txt", sep="\t", header=TRUE)
+dim(lrn14)
+str(lrn14)
+#The data has 183 rows (observations) and 60 columns (variables)
+
+deep_questions <- c("D03", "D11", "D19", "D27", "D07", "D14", "D22", "D30","D06",  "D15", "D23", "D31")
+surface_questions <- c("SU02","SU10","SU18","SU26", "SU05","SU13","SU21","SU29","SU08","SU16","SU24","SU32")
+strategic_questions <- c("ST01","ST09","ST17","ST25","ST04","ST12","ST20","ST28")
+
+deep_columns <- select(lrn14, one_of(deep_questions))
+lrn14$deep <- rowMeans(deep_columns)
+surface_columns <- select(lrn14, one_of(surface_questions))
+lrn14$surf <- rowMeans(surface_columns)
+strategic_columns <- select(lrn14, one_of(strategic_questions))
+lrn14$stra <- rowMeans(strategic_columns)
+
+keep_columns <- c("gender","Age","Attitude", "deep", "stra", "surf", "Points")
+
+learning2014 <- select(lrn14, one_of(keep_columns))
+
+lrn14$attitude <- lrn14$Attitude / 10
+#Variables "deep", "stra" and "surf" are already scaled in the data.
+
+learning2014 <- filter(learning2014, Points > 0) 
+str(learning2014)
+
+?write.csv
+  
